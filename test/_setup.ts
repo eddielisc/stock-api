a4185e6776737cdbae59e829c312d5a7d30c0ae5
eddielisc <6848701+eddielisc@users.noreply.github.com>
2020-09-05 16:51:34 +0800

    feat: basic setup

--- test/_setup.ts
+++ test/_setup.ts
@@ -1,4 +1,5 @@
 // set up global namespace for worker environment
-import * as makeServiceWorkerEnv from 'service-worker-mock'
-declare var global: any
+import * as makeServiceWorkerEnv from "service-worker-mock"
+
+declare let global: unknown
 Object.assign(global, makeServiceWorkerEnv())
